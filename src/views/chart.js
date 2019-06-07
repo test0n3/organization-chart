ab4a2d1b9e8247eb6732a7e824139f9a12aff4e7
Ricardo <ry.yrupailla@gmail.com>
2019-06-06 19:07:33 -0500

    Add hardcoded data

--- src/views/chart.js
+++ src/views/chart.js
@@ -2,6 +2,33 @@
 import React from "react";
 import { jsx } from "@emotion/core";
 
+const initialColaborators = JSON.parse(
+  localStorage.getItem("colaborators") ||
+    JSON.stringify([
+      {
+        id: 0,
+        name: "Carlos Ayala",
+        position: "CEO",
+        parent: "",
+        children: []
+      },
+      {
+        id: 1,
+        name: "Mayra",
+        position: "Product Manager",
+        parent: 0,
+        children: []
+      },
+      {
+        id: 2,
+        name: "Deivy Conde",
+        position: "Tech Lead",
+        parent: 0,
+        children: []
+      }
+    ])
+);
+
 function ChartView() {
   return <div>ChartView</div>;
 }
