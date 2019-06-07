636421a91a13c7b9b42b75cddec37ea4a520b3c8
byteconde <iamdeyvi@gmail.com>
2019-06-07 11:36:39 -0500

    Add testing company component

---
+++ src/components/create-company.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "@emotion/core";
+
+function CreateCompany(){
+  return(
+    <h1>Hello</h1>
+  );
+}
+
+export default CreateCompany;
