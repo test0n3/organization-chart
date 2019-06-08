95938aab8d6854439de822f0232e8204b392d2e3
byteconde <iamdeyvi@gmail.com>
2019-06-08 00:21:24 -0500

    Add CSS Styles in React for CEO

--- src/components/input.js
+++ src/components/input.js
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import React from 'react';
+import { jsx } from "@emotion/core";
 
 function InputNameCeo(props) {
   function handleTextChange(event) {
@@ -7,11 +9,30 @@ function InputNameCeo(props) {
   }
   return (
   <div>
-    <form onSubmit={handleTextChange}>
-      <input
-        name="ceo"
-        type="text"
-        placeholder="Ceo's name..."
+    <form css={{
+      display: "flex",
+      flexDirection: "column",
+      width: "400px",
+      boxShadow: "0 20px 25px -5px rgba(0, 0, 0, 0.5)",
+      padding: "1.5em",
+      background: "rgba(22, 22, 22, 0.57)",
+      borderRadius: "0.5em"
+    }}
+    onSubmit={handleTextChange}>
+      <input 
+      css={{
+        fontSize: "20px",
+        padding: "10px",
+        borderRadius: "4px",
+        outline: "none",
+        border: "none",
+        borderBottom: "1px solid #ddd",
+        width: "calc(100% - 10px - 10px)",
+        textAlign: "center"
+      }}
+      name="ceo"
+      type="text"
+      placeholder="Ceo's Name"
       />
     </form>
     </div>  
