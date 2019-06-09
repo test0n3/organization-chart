c48b34edd3f7f9838fa6689ee0be279ed4394665
Diego Cuevas <diegocuevas579@gmail.com>
2019-06-08 21:39:33 -0500

    Make ceo view responsive

--- src/components/input.js
+++ src/components/input.js
@@ -7,6 +7,10 @@ function InputNameCeo(props) {
     event.preventDefault();
     props.onCeo(event.target.elements.ceo.value);
   }
+
+  const breakpoints = [320, 375, 425, 768];
+  const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`); 
+
   return (
   <div>
     <form css={{
@@ -16,7 +20,23 @@ function InputNameCeo(props) {
       boxShadow: "0 20px 25px -5px rgba(0, 0, 0, 0.5)",
       padding: "1.5em",
       background: "rgba(22, 22, 22, 0.57)",
-      borderRadius: "0.5em"
+      borderRadius: "0.5em",
+      [mq[0]]: {
+        width: '250px',
+        padding: '0'
+      },
+      [mq[1]]: {
+        width: '300px',
+        padding: '0'
+      },
+      [mq[2]]: {
+        width: '350px',
+        padding: '0'
+      },
+      [mq[3]]: {
+        width: '400px',
+        padding: '0'
+      }
     }}
     onSubmit={handleTextChange}>
       <input 
